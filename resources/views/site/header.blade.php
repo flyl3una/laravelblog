a8728478d9d70956e8c88e3e422d232e4856b36e
luna <fly_luna@outlook.com>
2017-08-03 23:24:13 +0800

    white

--- resources/views/site/header.blade.php
+++ resources/views/site/header.blade.php
@@ -3,19 +3,19 @@
         <div class="container-fluid">
             <nav class="navbar site-navbar">
                 <div class="navbar-header navbar-nav">
-                    <a href="{{ route('blog.index') }}" class="navbar-brand ">Laravel Blog</a>
+                    <a href="{{ route('blog.index') }}" class="navbar-brand white-color">Laravel Blog</a>
                 </div>
                 <div class="navbar-collapse collapse">
                     <ul class="nav navbar-nav">
                         <li>
-                            <a href="{{ route("blog.index") }}" class="btn">归档</a>
+                            <a href="{{ route("blog.index") }}" class="btn white-color">归档</a>
                         </li>
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
                         <li>
-                            <a href="{{ url('login') }}">登陆</a>
+                            <a href="{{ url('login') }}" class="white-color">登陆</a>
                         </li>
-                        <li><a href="{{ url('register') }}">注册</a> </li>
+                        <li><a href="{{ url('register') }}" class="white-color">注册</a> </li>
                     </ul>
                 </div>
 
