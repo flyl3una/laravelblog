155d2856a1e9f1ec8c0d52c43a0316bf95b9fe26
luna <fly_luna@outlook.com>
2017-08-21 18:10:00 +0800

    重构界面

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -1,26 +1,28 @@
 @section('header')
-<div class="container-fluid" style="background-color: #1f648b">
-    <nav class="navbar site-navbar">
-        <div class="navbar-header navbar-nav">
-            <a href="{{ route('home.index') }}" class="navbar-brand white-color">Laravel Blog</a>
-        </div>
-        <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-                <li>
-                    <a href="{{ route("home.index") }}" class="btn white-color">归档</a>
-                </li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li>
-                    <a href="{{ route('login') }}" class="white-color">登陆</a>
-                </li>
-                <li><a href="{{ route('register') }}" class="white-color">注册</a> </li>
-                <li><a href="{{ route('logout') }}" class="white-color">登出</a> </li>
-                <li><a href="{{ route('admin.index') }}" class="white-color">后台</a> </li>
-            </ul>
-        </div>
+    <div class="container-fluid admin-header">
+        <div class="row">
+            <nav class="navbar navbar-brand">
+                <div class="navbar-header navbar-nav">
+                    <a href="{{ route('home.index') }}" class="navbar-brand white-color">Laravel Blog</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li>
+                            <a href="{{ route("home.index") }}" class="btn white-color">归档</a>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="{{ route('login') }}" class="white-color">登陆</a>
+                        </li>
+                        <li><a href="{{ route('register') }}" class="white-color">注册</a></li>
+                        <li><a href="{{ route('logout') }}" class="white-color">登出</a></li>
+                        <li><a href="{{ route('admin.index') }}" class="white-color">后台</a></li>
+                    </ul>
+                </div>
 
-    </nav>
-</div>
+            </nav>
+        </div>
+    </div>
 
 @endsection
--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -1,9 +1,13 @@
 <html>
 <head>
     <title>Laravel Blog</title>
-    <link href="/css/bootstrap.css" rel="stylesheet">
-    <script src="/js/jquery-3.2.1.min.js"></script>
+    <link href="/vendors/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+    <link href="/vendors/bootstrap-material-design/dist/css/bootstrap-material-design.css" rel="stylesheet">
+    <link href="/vendors/bootstrap-material-design/dist/css/ripples.css" rel="stylesheet">
+    <link rel="stylesheet" href="/vendors/font-awesome/css/font-awesome.min.css">
 
+    {{--<link rel="stylesheet" href="/css/blog.css">--}}
+    <link rel="stylesheet" href="/css/my.css">
     @yield("css")
     <meta name="_token" content="{{ csrf_token() }}"/>
     {{--<link href="/css/boostrap-theme.css" rel="stylesheet">--}}
@@ -21,7 +25,13 @@
 </div>
 
 {{--@extends("layouts.footer")--}}
-<script src="/js/bootstrap.min.js"></script>
+<script src="/vendors/jquery/dist/jquery.min.js"></script>
+<script src="/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="/vendors/bootstrap-material/dist/material.js"></script>
+<script src="/vendors/bootstrap-material/dist/ripples.js"></script>
+<script>
+    $.material.init();
+</script>
 @yield("js")
 </body>
 </html>
